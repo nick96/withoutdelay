de7ff05b571287b5f6ff2ceaa528a42a3d16223b
Nick Spain <nicholas.spain@stileeducation.com>
2020-07-11 18:31:28 +1000

    Stub of temporal tests

--- temporal/tests/test_temporal.py
+++ temporal/tests/test_temporal.py
@@ -2,4 +2,12 @@ from temporal import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.0.1"
+
+
+def test_tag_timex():
+    pass
+
+
+def test_timex_to_absolute_time():
+    pass
