9fcc76f02fdcb2f99f444784f1691081fe5f02fc
Nick Spain <nicholas.spain@stileeducation.com>
2020-07-11 20:45:36 +1000

    Clean up logging in grpc gateway

--- temporal/gateway/main.go
+++ temporal/gateway/main.go
@@ -26,22 +26,20 @@ import (
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	log.Printf("gRPC server endpoint: %s", grpcServerEndpoint)
-	log.Printf("port: %s", port)
   
 	// Register gRPC server endpoint
 	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithInsecure()}
-	log.Printf("Proxying to gRPC server at %s", grpcServerEndpoint)
+	log.Printf("Proxying to gRPC server at %s", *grpcServerEndpoint)
 	err := gw.RegisterTemporalServiceHandlerFromEndpoint(ctx, mux,  *grpcServerEndpoint, opts)
 	if err != nil {
 	  return err
 	}
   
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
-	log.Printf("Running on port %s", port)
-	return http.ListenAndServe(fmt.Sprintf(":%s", port), mux)
+	log.Printf("Running on port %s", *port)
+	return http.ListenAndServe(fmt.Sprintf(":%s", *port), mux)
   }
   
   func main() {
