95f86ce4905ce9fb19c7fe501e1bf930cb2ddf82
Nick Spain <nicholas.spain@stileeducation.com>
2020-07-11 20:48:17 +1000

    Implement basic tagging behaviour in temporal service

--- temporal/temporal/server.py
+++ temporal/temporal/server.py
@@ -2,22 +2,41 @@
 
 from concurrent import futures
 import logging
+import re
+from datetime import datetime
 
 import grpc
 
 from temporal import temporal_service_pb2
 from temporal import temporal_service_pb2_grpc
+from temporal import timex
 
 
 class TemporalServiceServicer(temporal_service_pb2_grpc.TemporalServiceServicer):
     """Implementation of GRPC service methods."""
 
     def TagTimex(self, request, context):
-        yield temporal_service_pb2.TimexTag(
-            timex="", pos=0,
-        )
+        tagged = timex.tag(request.text)
+        timex_matches = re.finditer(r"<TIMEX2>(.+)<\/TIMEX2>", tagged)
+        for timex_match in timex_matches:
+            timex = timex_match.group(1)
+            pos = timex_match.pos + len("<TIMEX2>")
+            yield temporal_service_pb2.TimexTag(
+                timex=timex, pos=pos,
+            )
 
     def TimexToAbsolute(self, request, context):
+        logger = logging.getLogger("timex_to_abs")
+
+        tagged = timex.tag(request.txt)
+        base_time = datetime.now()
+        ground = timex.ground(tagged, base_time)
+        
+        match = re.match(r"<TIMEX2 val=(.+)>(.+)<\/TIMEX2>")
+
+        logger.info(f"Grounded time: {match.group(1)}")
+        logger.info(f"Timex: {match.group(2)}")
+
         return temporal_service_pb2.TimexToAbsoluteResponse(
             input=request.timex, absoluteDateTime=0,
         )
